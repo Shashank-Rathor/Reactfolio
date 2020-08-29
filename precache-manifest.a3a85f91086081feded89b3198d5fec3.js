964b2570351a79c913fb99b44d7bd7204e58686f
Shashank Rathor <shashankrathor180@gmail.com>
2020-08-29 22:32:14 +0530

    Updates

---
+++ precache-manifest.a3a85f91086081feded89b3198d5fec3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfbdb35043e3c914bcf67e5bbbf0ae6",
+    "url": "/Reactfolio/index.html"
+  },
+  {
+    "revision": "6e9be0dad0a210e0a3a8",
+    "url": "/Reactfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "12ce3ccc7ed5b9e840ed",
+    "url": "/Reactfolio/static/css/main.970aa2a5.chunk.css"
+  },
+  {
+    "revision": "6e9be0dad0a210e0a3a8",
+    "url": "/Reactfolio/static/js/2.a481519f.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Reactfolio/static/js/2.a481519f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ce3ccc7ed5b9e840ed",
+    "url": "/Reactfolio/static/js/main.f105a4a5.chunk.js"
+  },
+  {
+    "revision": "d279430cd433889f325a",
+    "url": "/Reactfolio/static/js/runtime-main.4909c548.js"
+  },
+  {
+    "revision": "dd48ff59d2538364aab958c6836b19b5",
+    "url": "/Reactfolio/static/media/CoronaTracker.dd48ff59.png"
+  },
+  {
+    "revision": "ec2c8da78e70d3093f76e3c314157d78",
+    "url": "/Reactfolio/static/media/DiceGame.ec2c8da7.png"
+  },
+  {
+    "revision": "21c864560463e7954848fe8394ac4ad5",
+    "url": "/Reactfolio/static/media/Portfolio.21c86456.png"
+  },
+  {
+    "revision": "075bee6d5d73a23bf2096a989a5cd7a0",
+    "url": "/Reactfolio/static/media/Tindog.075bee6d.png"
+  },
+  {
+    "revision": "e447954c6913201b57ceb06de607b983",
+    "url": "/Reactfolio/static/media/background.e447954c.webp"
+  },
+  {
+    "revision": "da1e4220809499b3296a20c8088b1c7f",
+    "url": "/Reactfolio/static/media/bootstrap-4.da1e4220.png"
+  },
+  {
+    "revision": "ddc28dedf15310519c0b3074cb9272ee",
+    "url": "/Reactfolio/static/media/coding.ddc28ded.jpg"
+  },
+  {
+    "revision": "dbf26442c2e23da459cc6b34e5e2e83d",
+    "url": "/Reactfolio/static/media/comp.dbf26442.jpg"
+  },
+  {
+    "revision": "8cf8f549644e217248043be353a1b273",
+    "url": "/Reactfolio/static/media/css3.8cf8f549.png"
+  },
+  {
+    "revision": "95471a07f1c7fafc6f248b3941362e86",
+    "url": "/Reactfolio/static/media/git-icon.95471a07.png"
+  },
+  {
+    "revision": "6d593e4100f01541df21db927e9ba687",
+    "url": "/Reactfolio/static/media/github.6d593e41.png"
+  },
+  {
+    "revision": "77013a0cdcdc8c1e3fb0af85f97aae71",
+    "url": "/Reactfolio/static/media/heroku.77013a0c.png"
+  },
+  {
+    "revision": "cc0a9cf53fd25f09465fce46616ba2b2",
+    "url": "/Reactfolio/static/media/html-5.cc0a9cf5.png"
+  },
+  {
+    "revision": "14e7358f0ef9a02b883dedc562c50c48",
+    "url": "/Reactfolio/static/media/java.14e7358f.png"
+  },
+  {
+    "revision": "6c370a66e84b694fee5d583cac848eb0",
+    "url": "/Reactfolio/static/media/javascript.6c370a66.png"
+  },
+  {
+    "revision": "582e763f8e52244151961bc7b62ba298",
+    "url": "/Reactfolio/static/media/lappy.582e763f.jpg"
+  },
+  {
+    "revision": "b7bbd065807330f5912b44932d9db42c",
+    "url": "/Reactfolio/static/media/material-ui-1.b7bbd065.png"
+  },
+  {
+    "revision": "d0feacb735778f2317e35315ea23da3a",
+    "url": "/Reactfolio/static/media/mssql.d0feacb7.png"
+  },
+  {
+    "revision": "651f48931edea3c8b979410b8d6713d5",
+    "url": "/Reactfolio/static/media/react-bootstrap.651f4893.png"
+  },
+  {
+    "revision": "5fef7a630d6e2c944be7183c974c8340",
+    "url": "/Reactfolio/static/media/react-router.5fef7a63.png"
+  },
+  {
+    "revision": "4403ecefd27c87bb270601741ff0eac7",
+    "url": "/Reactfolio/static/media/react.4403ecef.png"
+  },
+  {
+    "revision": "ea3346315f40204d337c44ecbaa9637f",
+    "url": "/Reactfolio/static/media/redux.ea334631.png"
+  },
+  {
+    "revision": "aa961ce0b67a5d8f2e1a2898c9a76253",
+    "url": "/Reactfolio/static/media/self.aa961ce0.jpg"
+  },
+  {
+    "revision": "4cd6e85b99f0039919e0888bc76017fd",
+    "url": "/Reactfolio/static/media/white.4cd6e85b.jpg"
+  }
+]);
